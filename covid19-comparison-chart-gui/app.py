bec5dfe69c8949215a7782c372f18873d32ae78f
Krzysztof Kwiatkowski <kthateyo@gmail.com>
2020-05-22 10:42:11 +0200

    browser fallback

--- covid19-comparison-chart-gui/app.py
+++ covid19-comparison-chart-gui/app.py
@@ -1,5 +1,13 @@
-import eel
+import eel, sys, platform
 from python.data import getAllCountries, getData
 
 eel.init('web')
-eel.start('main.html', size=(1280, 720))
+
+try:
+    eel.start('main.html', mode='chrome', port=0, size=(1280, 720))
+except EnvironmentError:
+    if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
+        eel.start('main.html', mode='edge', port=0, size=(1280, 720))
+    else:
+        raise
+    
